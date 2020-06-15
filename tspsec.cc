98d1a6bbe159c7ce7602dcfbeb6017fedde2a091
Xavier <xaavier@protonmail.com>
2020-06-15 17:37:15 +0200

    Process 0 sharing nodes to work across all tasks

--- tspsec.cc
+++ tspsec.cc
@@ -1,6 +1,3 @@
-/* ******************************************************************** */
-/*               Algoritmo Branch-And-Bound Secuencial                  */
-/* ******************************************************************** */
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
@@ -10,36 +7,124 @@
 using namespace std;
  
 unsigned int NCIUDADES;
- 
-main (int argc, char **argv) {
-        MPI::Init(argc,argv);
+int size, rango;
+int tagTrabajo = 50;
+MPI_Datatype nodoDatatype;
+MPI_Request request;
+MPI_Status status;
+
+struct tNodoPar {
+	int ci;
+	int incl[100];
+	int orig_excl;
+	int dest_excl[100];
+} nodoPar;
+
+void asignarTrabajo(tPila *pila, tNodo *nodo) {
+	int tarea;
+	int flag = 1;
+	while(flag == 1) {
+		//Recibimos los mensajes pendientes
+		if(MPI_Irecv(&tarea, 1, MPI_INT, MPI_ANY_SOURCE, tagTrabajo, MPI_COMM_WORLD, &request) == MPI_SUCCESS) {
+			//MPI_TEST comprueba si hemos recibido algun mensaje
+			MPI_Test(&request, &flag, &status);
+			//Enviamos trabajo al proceso que hizo la peticion
+			if(flag == 1) {
+				//Sacar el ultimo elemento de la pila y copiarlo al struct
+				PilaPop(pila, nodo);
+				//Adaptar el formato del struct para enviarlo
+				nodoPar.ci = nodo->ci;
+				for(int i=0; i<NCIUDADES; ++i) {
+					nodoPar.incl[i] = nodo->incl[i];
+				}
+				for(int j=0; j<NCIUDADES-2; ++j) {
+					nodoPar.dest_excl[j] = nodo->dest_excl[j];
+				}
+				//Enviar el struct a la tarea recibida
+				MPI_Send(&nodoPar, 1, nodoDatatype, tarea, tagTrabajo, MPI_COMM_WORLD);
+			}
+		}
+		else {
+			exit(2);
+		}
+	}
+}
+
+void obtenerTrabajo(tPila *pila, tNodo *nodo) {
+	//Enviar mensaje con el numero de tarea
+	MPI_Send(&rango, 1, MPI_INT, 0, tagTrabajo, MPI_COMM_WORLD);
+	//Esperar recepcion del struct
+	if(MPI_Recv(&nodoPar, 1, nodoDatatype, 0, tagTrabajo, MPI_COMM_WORLD, &status) == MPI_SUCCESS) {
+		//Adaptar el formato del struct y guardarlo
+		nodo->ci = nodoPar.ci;
+		for(int i=0; i<NCIUDADES; ++i) {
+			nodo->incl[i] = nodoPar.incl[i];
+		}
+		for(int j=0; j<NCIUDADES-2; ++j) {
+			nodo->dest_excl[j] = nodoPar.dest_excl[j];
+		}
+		//Push a la pila con el nodo obtenido
+		PilaPush(pila, nodo);
+	}
+	else {
+		exit(2);
+	}
+}
+
+int main (int argc, char **argv) {
+    MPI::Init(argc,argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+  	MPI_Comm_rank(MPI_COMM_WORLD, &rango);
+
 	switch (argc) {
-		case 3:		NCIUDADES = atoi(argv[1]);
-					break;
-		default:	cerr << "La sintaxis es: bbseq <tamaño> <archivo>" << endl;
-					exit(1);
-					break;
+		case 3:
+			NCIUDADES = atoi(argv[1]);
+			break;
+		default:
+			cerr << "La sintaxis es: bbseq <tamaño> <archivo>" << endl;
+			exit(1);
+			break;
 	}
  
 	int** tsp0 = reservarMatrizCuadrada(NCIUDADES);
-	tNodo	nodo,         // nodo a explorar
-			lnodo,        // hijo izquierdo
-			rnodo,        // hijo derecho
-			solucion;     // mejor solucion
-	bool activo,        // condicion de fin
-		nueva_U;       // hay nuevo valor de c.s.
-	int  U;             // valor de c.s.
-	tPila pila;         // pila de nodos a explorar
- 
-	U = INFINITO;                  // inicializa cota superior
-	InicNodo (&nodo);              // inicializa estructura nodo
-	PilaInic (&pila);              // inicializa pila
-	LeerMatriz (argv[2], tsp0);    // lee matriz de fichero
+	tNodo	nodo,		// nodo a explorar
+			lnodo,		// hijo izquierdo
+			rnodo,		// hijo derecho
+			solucion;	// mejor solucion
+	bool activo,		// condicion de fin
+		nueva_U;		// hay nuevo valor de c.s.
+	int  U;				// valor de c.s.
+	tPila pila;			// pila de nodos a explorar
+	U = INFINITO;		// inicializa cota superior
+
+	//Estructura de datos MPI
+	int blocklenghts[4] = {1, 100, 1, 100};
+	MPI_Datatype types[4] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT};
+	MPI_Aint offsets[4];
+	offsets[0] = offsetof(tNodoPar, ci);
+	offsets[1] = offsetof(tNodoPar, incl);
+	offsets[2] = offsetof(tNodoPar, orig_excl);
+	offsets[3] = offsetof(tNodoPar, dest_excl);
+	MPI_Type_struct(4, blocklenghts, offsets, types, &nodoDatatype);
+	MPI_Type_commit(&nodoDatatype);
+
+	InicNodo (&nodo);	// inicializa estructura nodo
+	PilaInic (&pila);	// inicializa pila
 	double t=MPI::Wtime();
-	
+
+	//El proceso master lee la matriz de fichero
+	if(rango == 0) {
+		LeerMatriz (argv[2], tsp0);
+	}
+	//Compartimos la matriz con todos los procesos
+	MPI_Bcast(&tsp0[0][0], NCIUDADES*NCIUDADES, MPI_INT, 0, MPI_COMM_WORLD);
 	activo = !Inconsistente(tsp0);
+	//Los procesos piden trabajo al master
+	if(rango != 0) {
+		obtenerTrabajo(&pila, &nodo);
+	}
 	
-	while (activo) {       // ciclo del Branch&Bound
+	while (activo) {
 		Ramifica (&nodo, &lnodo, &rnodo, tsp0);
 		nueva_U = false;
 		if (Solucion(&rnodo)) {
@@ -76,10 +161,27 @@ main (int argc, char **argv) {
 		}
 		if (nueva_U) PilaAcotar (&pila, U);
 		activo = PilaPop (&pila, &nodo);
+
+		//El proceso master atiende las peticiones de trabajo
+		if(rango == 0) {
+			asignarTrabajo(&pila, &nodo);
+		}
+		//Si la pila esta vacia pedimos trabajo al proceso master
+		else {
+			if(PilaVacia(&pila)) {
+				obtenerTrabajo(&pila, &nodo);
+			}
+		}
 	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
  	t=MPI::Wtime()-t;
- 	
-    	MPI::Finalize();
-	EscribeSolucion(&solucion, t);
+    //El proceso master escribe la solucion
+	if(rango == 0) {
+		EscribeSolucion(&solucion, t);
+	}
 	liberarMatriz(tsp0);
+
+	MPI::Finalize();
+	return 0;
 }
